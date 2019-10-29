1db853a0fa4cd0c2b5657480a0e1a2f85af98533
kevinyee628 <553407655@qq.com>
2019-10-29 20:37:17 +0800

    centerloss

--- model/network/center_loss.py
+++ model/network/center_loss.py
@@ -15,12 +15,13 @@ class CenterLoss(nn.Module):
     """
 
     # feat_dim = 3
-    def __init__(self, num_classes=10, feat_dim=3, use_gpu=True):
+    def __init__(self, num_classes=10, feat_dim=3, use_gpu=True, margin):
         super(CenterLoss, self).__init__()
         self.num_classes = num_classes
         self.feat_dim = feat_dim
         # self.margin = margin
         self.use_gpu = use_gpu
+        self.margin = margin
 
         if self.use_gpu:
             self.centers = nn.Parameter(torch.randn(self.num_classes, self.feat_dim).cuda())
@@ -33,17 +34,35 @@ class CenterLoss(nn.Module):
             x: feature matrix with shape (batch_size, feat_dim).
             labels: ground truth labels with shape (batch_size).
         """
+
+        n, m, d = x.size()
+        hp_mask = (labels.unsqueeze(1) == labels.unsqueeze(2)).byte().view(-1)
+        hn_mask = (labels.unsqueeze(1) != labels.unsqueeze(2)).byte().view(-1)
+
         batch_size = x.size(0)
-        distmat = torch.pow(x, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
+        dist = self.basenet(x)
+        mean = dist.mean(1).mean(1)
+        dist = dist.view(-1)
+
+        # TODO:降低feature维度
+        full_hp_dist = torch.masked_select(dist, hp_mask).view(n, m, -1, 1)
+        full_hn_dist = torch.masked_select(dist, hn_mask).view(n, m, 1, -1)
+
+        distmat = torch.pow(full_hp_dist, 2).sum(dim=1, keepdim=True).expand(batch_size, self.num_classes) + \
                   torch.pow(self.centers, 2).sum(dim=1, keepdim=True).expand(self.num_classes, batch_size).t()
-        distmat.addmm_(1, -2, x, self.centers.t())
+
+        distmat.addmm_(1, -2, full_hp_dist, self.centers.t())
 
         classes = torch.arange(self.num_classes).long()
-        if self.use_gpu: classes = classes.cuda()
+        if self.use_gpu:
+            classes = classes.cuda()
         labels = labels.unsqueeze(1).expand(batch_size, self.num_classes)
         mask = labels.eq(classes.expand(batch_size, self.num_classes))
 
         dist = distmat * mask.float()
+        # TODO:降为1D
+        # full_loss_metric = F.relu(self.margin + dist).view(n, -1)
+
         loss = dist.clamp(min=1e-12, max=1e+12).sum() / batch_size
 
         return loss
